95287950de0754954215ac5f5fdc85313ef13fc6
David Morgan <David.Morgan@advancedsecure.co.uk>
2025-03-04 22:58:46 +0000

    Pass first generate test

--- FakeMaker.Core/Generator.cs
+++ FakeMaker.Core/Generator.cs
@@ -4,6 +4,12 @@ public class Generator : IGenerator
 {
     public IEnumerable<Record> Generate(int count, Configuration configuration)
     {
-        throw new NotImplementedException();
+        for (var i = 0; i < count; ++i)
+            yield return GenerateRecord(configuration);
+    }
+
+    private static Record GenerateRecord(Configuration configuration)
+    {
+        return new Record(Fields: []);
     }
 }
