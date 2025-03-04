37648879aecc8d1b1fcc74f21a4d949689565bd8
David Morgan <David.Morgan@advancedsecure.co.uk>
2025-03-04 21:45:56 +0000

    Add toolstrip buttons

--- FakeMaker/MainForm.Designer.cs
+++ FakeMaker/MainForm.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            toolStrip1 = new ToolStrip();
+            configureButton = new ToolStripButton();
+            generateButton = new ToolStripButton();
+            exportButton = new ToolStripButton();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.GripStyle = ToolStripGripStyle.Hidden;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { configureButton, generateButton, exportButton });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip";
+            // 
+            // configureButton
+            // 
+            configureButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            configureButton.Image = (Image)resources.GetObject("configureButton.Image");
+            configureButton.ImageTransparentColor = Color.Magenta;
+            configureButton.Name = "configureButton";
+            configureButton.Size = new Size(64, 22);
+            configureButton.Text = "Configure";
+            // 
+            // generateButton
+            // 
+            generateButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            generateButton.Image = (Image)resources.GetObject("generateButton.Image");
+            generateButton.ImageTransparentColor = Color.Magenta;
+            generateButton.Name = "generateButton";
+            generateButton.Size = new Size(58, 22);
+            generateButton.Text = "Generate";
+            // 
+            // exportButton
+            // 
+            exportButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            exportButton.Image = (Image)resources.GetObject("exportButton.Image");
+            exportButton.ImageTransparentColor = Color.Magenta;
+            exportButton.Name = "exportButton";
+            exportButton.Size = new Size(44, 22);
+            exportButton.Text = "Export";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(toolStrip1);
             Name = "MainForm";
             Text = "FakeMaker";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton configureButton;
+        private ToolStripButton generateButton;
+        private ToolStripButton exportButton;
     }
 }
