eeb13dd987669d4ecbd9c511e7c31ed6bc98a2a0
David Morgan <David.Morgan@advancedsecure.co.uk>
2025-03-05 22:16:45 +0000

    Implement configuration deserialization

---
+++ FakeMaker.Core.Tests.Unit/ConfigurationSerializerTests/LoadTests.cs
@@ -0,0 +1,54 @@
+﻿namespace FakeMaker.Core.Tests.Unit.ConfigurationSerializerTests;
+
+public class LoadTests
+{
+    [Fact]
+    public void FileIsEmpty_ReturnsEmptyConfiguration()
+    {
+        // Arrange
+        using var stream = new MemoryStream();
+
+        // Act
+        var result = ConfigurationSerializer.Load(stream);
+
+        // Assert
+        Assert.Empty(result.Columns);
+    }
+
+    [Fact]
+    public void FileHasTwoColumns_ConfigurationContainsCorrectColumns()
+    {
+        // Arrange
+        using var stream = new MemoryStream();
+        using var writer = new StreamWriter(stream);
+
+        writer.Write(
+            """
+            <configuration>
+                <column name="first_name" type="FirstName" />
+                <column name="last_name" type="LastName" />
+            </configuration>
+            """);
+
+        writer.Flush();
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+        // Act
+        var result = ConfigurationSerializer.Load(stream);
+
+        // Assert
+        Assert.Collection(
+            result.Columns,
+            column1 =>
+            {
+                Assert.Equal("first_name", column1.Name);
+                Assert.Equal(DataType.FirstName, column1.Type);
+            },
+            column2 =>
+            {
+                Assert.Equal("last_name", column2.Name);
+                Assert.Equal(DataType.LastName, column2.Type);
+            });
+    }
+}
---
+++ FakeMaker.Core/ConfigurationSerializer.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml;
+
+namespace FakeMaker.Core;
+
+public static class ConfigurationSerializer
+{
+    public static Configuration Load(Stream stream)
+    {
+        var xmlDocument = new XmlDocument();
+
+        var configuration = new Configuration();
+
+        try
+        {
+            xmlDocument.Load(stream);
+        }
+        catch
+        {
+            return configuration;
+        }
+
+        if (xmlDocument.DocumentElement is null)
+            return configuration;
+
+        var columnNodes = xmlDocument.DocumentElement.GetElementsByTagName("column").Cast<XmlNode>();
+
+        foreach (var columnNode in columnNodes)
+        {
+            if (columnNode.Attributes is null)
+                continue;
+
+            var nameNode = columnNode.Attributes.GetNamedItem("name");
+            var typeNode = columnNode.Attributes.GetNamedItem("type");
+
+            if (nameNode is null || typeNode is null)
+                continue;
+
+            if (string.IsNullOrWhiteSpace(nameNode.Value))
+                continue;
+
+            if (!Enum.TryParse<DataType>(typeNode.Value, out var dataType))
+                continue;
+
+            var column = new Column()
+            {
+                Name = nameNode.Value,
+                Type = dataType,
+            };
+
+            configuration.Columns.Add(column);
+        }
+
+        return configuration;
+    }
+}
