a90b1815f17c84d874ea6fad695945871accc790
David Morgan <David.Morgan@advancedsecure.co.uk>
2025-03-05 08:08:29 +0000

    Implement data types

--- FakeMaker.Core.Tests.Unit/GeneratorTests/GenerateTests.cs
+++ FakeMaker.Core.Tests.Unit/GeneratorTests/GenerateTests.cs
@@ -48,4 +48,156 @@ public class GenerateTests
         // Assert
         Assert.All(result, x => Assert.Equal(columnCount, x.Fields.Length));
     }
+
+    [Fact]
+    public void ConfigurationHasFirstNameColumn_RecordsHaveFirstNameField()
+    {
+        // Arrange
+        var configuration = new Configuration()
+        {
+            Columns =
+            [
+                new Column() { Name = "first_name", Type = DataType.FirstName },
+            ],
+        };
+
+        var generator = new Generator();
+
+        // Act
+        var result = generator.Generate(10, configuration);
+
+        // Assert
+        Assert.All(result, x =>
+        {
+            Assert.NotEmpty(x.Fields[0]);
+            Assert.True(char.IsUpper(x.Fields[0][0]));
+            Assert.All(x.Fields[0][1..], y => char.IsUpper(y));
+        });
+    }
+
+    [Fact]
+    public void ConfigurationHasLastNameColumn_RecordsHaveLastNameField()
+    {
+        // Arrange
+        var configuration = new Configuration()
+        {
+            Columns =
+            [
+                new Column() { Name = "last_name", Type = DataType.LastName },
+            ],
+        };
+
+        var generator = new Generator();
+
+        // Act
+        var result = generator.Generate(10, configuration);
+
+        // Assert
+        Assert.All(result, x =>
+        {
+            Assert.NotEmpty(x.Fields[0]);
+            Assert.True(char.IsUpper(x.Fields[0][0]));
+            Assert.All(x.Fields[0][1..], y => char.IsUpper(y));
+        });
+    }
+
+    [Fact]
+    public void ConfigurationHasEnrolmentNumberColumn_RecordsHaveEnrolmentNumberField()
+    {
+        // Arrange
+        var configuration = new Configuration()
+        {
+            Columns =
+            [
+                new Column() { Name = "enrolment_number", Type = DataType.EnrolmentNumber },
+            ],
+        };
+
+        var generator = new Generator();
+
+        // Act
+        var result = generator.Generate(10, configuration);
+
+        // Assert
+        Assert.All(result, x =>
+        {
+            Assert.Equal(8, x.Fields[0].Length);
+            Assert.All(x.Fields[0], y => char.IsDigit(y));
+        });
+    }
+
+    [Fact]
+    public void ConfigurationHasEmailAddressColumn_RecordsHaveEmailAddressField()
+    {
+        // Arrange
+        var configuration = new Configuration()
+        {
+            Columns =
+            [
+                new Column() { Name = "email_address", Type = DataType.EmailAddress },
+            ],
+        };
+
+        var generator = new Generator();
+
+        // Act
+        var result = generator.Generate(10, configuration);
+
+        // Assert
+        Assert.All(result, x =>
+        {
+            Assert.NotEmpty(x.Fields[0]);
+            Assert.Contains('@', x.Fields[0]);
+        });
+    }
+
+    [Fact]
+    public void ConfigurationHasDateRecentColumn_RecordsHaveDateRecentField()
+    {
+        // Arrange
+        var configuration = new Configuration()
+        {
+            Columns =
+            [
+                new Column() { Name = "date_recent", Type = DataType.DateRecent },
+            ],
+        };
+
+        var generator = new Generator();
+
+        // Act
+        var result = generator.Generate(10, configuration);
+
+        // Assert
+        Assert.All(result, x =>
+        {
+            Assert.True(DateOnly.TryParse(x.Fields[0], out var dateRecent));
+            Assert.True(dateRecent <= DateOnly.FromDateTime(DateTime.Today));
+        });
+    }
+
+    [Fact]
+    public void ConfigurationHasDateSoonColumn_RecordsHaveDateSoonField()
+    {
+        // Arrange
+        var configuration = new Configuration()
+        {
+            Columns =
+            [
+                new Column() { Name = "date_soon", Type = DataType.DateSoon },
+            ],
+        };
+
+        var generator = new Generator();
+
+        // Act
+        var result = generator.Generate(10, configuration);
+
+        // Assert
+        Assert.All(result, x =>
+        {
+            Assert.True(DateOnly.TryParse(x.Fields[0], out var dateRecent));
+            Assert.True(dateRecent >= DateOnly.FromDateTime(DateTime.Today));
+        });
+    }
 }
--- FakeMaker.Core/Generator.cs
+++ FakeMaker.Core/Generator.cs
@@ -12,10 +12,24 @@ public class Generator : IGenerator
             yield return GenerateRecord(configuration);
     }
 
-    private static Record GenerateRecord(Configuration configuration)
+    private Record GenerateRecord(Configuration configuration)
     {
-        var fields = Enumerable.Repeat(string.Empty, configuration.Columns.Count);
+        var fields = configuration.Columns.Select(GenerateField);
 
         return new Record([.. fields]);
+    }
+
+    private string GenerateField(Column column)
+    {
+        return column.Type switch
+        {
+            DataType.FirstName => faker.Name.FirstName(),
+            DataType.LastName => faker.Name.LastName(),
+            DataType.EnrolmentNumber => faker.Commerce.Ean8(),
+            DataType.EmailAddress => faker.Internet.Email(),
+            DataType.DateRecent => faker.Date.RecentDateOnly().ToString(),
+            DataType.DateSoon => faker.Date.SoonDateOnly().ToString(),
+            _ => throw new NotImplementedException(),
+        };
     }
 }
