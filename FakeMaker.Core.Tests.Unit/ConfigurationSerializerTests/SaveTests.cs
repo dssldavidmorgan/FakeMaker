63229a0d4ec6b215f3e0a4aa0502dec52fc5407b
David Morgan <David.Morgan@advancedsecure.co.uk>
2025-03-05 22:50:31 +0000

    Implement configuration serialization

---
+++ FakeMaker.Core.Tests.Unit/ConfigurationSerializerTests/SaveTests.cs
@@ -0,0 +1,59 @@
+﻿namespace FakeMaker.Core.Tests.Unit.ConfigurationSerializerTests;
+
+public class SaveTests
+{
+    [Fact]
+    public void ConfigurationHasNoColumns_FileContainsRootNode()
+    {
+        // Arrange
+        using var stream = new MemoryStream();
+
+        var configuration = new Configuration();
+
+        // Act
+        ConfigurationSerializer.Save(configuration, stream);
+
+        // Assert
+        stream.Seek(0, SeekOrigin.Begin);
+        using var reader = new StreamReader(stream);
+        var result = reader.ReadToEnd();
+
+        Assert.Equal(
+            """
+            <configuration />
+            """,
+            result);
+    }
+
+    [Fact]
+    public void ConfigurationHasTwoColumns_FileContainsTwoColumnNodes()
+    {
+        // Arrange
+        using var stream = new MemoryStream();
+
+        var configuration = new Configuration()
+        {
+            Columns = [
+                new Column() { Name = "first_name", Type = DataType.FirstName },
+                new Column() { Name = "last_name", Type = DataType.LastName },
+            ],
+        };
+
+        // Act
+        ConfigurationSerializer.Save(configuration, stream);
+
+        // Assert
+        stream.Seek(0, SeekOrigin.Begin);
+        using var reader = new StreamReader(stream);
+        var result = reader.ReadToEnd();
+
+        Assert.Equal(
+            """
+            <configuration>
+                <column name="first_name" type="FirstName" />
+                <column name="last_name" type="LastName" />
+            </configuration>
+            """,
+            result);
+    }
+}
--- FakeMaker.Core/ConfigurationSerializer.cs
+++ FakeMaker.Core/ConfigurationSerializer.cs
@@ -52,4 +52,30 @@ public static class ConfigurationSerializer
 
         return configuration;
     }
+
+    public static void Save(Configuration configuration, Stream stream)
+    {
+        var newLine = Environment.NewLine;
+        var indentation = new string([.. Enumerable.Repeat(' ', 4)]);
+
+        var xmlDocument = new XmlDocument();
+        var documentElement = xmlDocument.CreateElement("configuration");
+        xmlDocument.AppendChild(documentElement);
+
+        if (configuration.Columns.Count > 0)
+            documentElement.AppendChild(xmlDocument.CreateWhitespace(newLine));
+
+        foreach (var column in configuration.Columns)
+        {
+            var columnElement = xmlDocument.CreateElement("column");
+            columnElement.SetAttribute("name", column.Name);
+            columnElement.SetAttribute("type", column.Type.ToString());
+
+            documentElement.AppendChild(xmlDocument.CreateWhitespace(indentation));
+            documentElement.AppendChild(columnElement);
+            documentElement.AppendChild(xmlDocument.CreateWhitespace(newLine));
+        }
+
+        xmlDocument.Save(stream);
+    }
 }
