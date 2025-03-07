816cbdb0f09144f46b76a2556bf7c546c3865c44
David Morgan <David.Morgan@advancedsecure.co.uk>
2025-03-07 08:57:49 +0000

    Implement custom configuration serialization

--- FakeMaker.Core.Tests.Unit/ConfigurationSerializerTests/SaveTests.cs
+++ FakeMaker.Core.Tests.Unit/ConfigurationSerializerTests/SaveTests.cs
@@ -56,4 +56,56 @@ public class SaveTests
             """,
             result);
     }
+
+    [Fact]
+    public void ConfigurationHasTwoCustomDataTypes_FileContainsTwoCustomDataTypeNodes()
+    {
+        // Arrange
+        using var stream = new MemoryStream();
+
+        var configuration = new Configuration()
+        {
+            CustomDataTypes = [
+                new CustomDataType()
+                {
+                    Name = "custom1",
+                    Values = ["0", "1", "2", "3", "4"],
+                },
+                new CustomDataType()
+                {
+                    Name = "custom2",
+                    Values = ["5", "6", "7", "8", "9"],
+                },
+            ],
+        };
+
+        // Act
+        ConfigurationSerializer.Save(configuration, stream);
+
+        // Assert
+        stream.Seek(0, SeekOrigin.Begin);
+        using var reader = new StreamReader(stream);
+        var result = reader.ReadToEnd();
+
+        Assert.Equal(
+            """
+            <configuration>
+                <customDataType name="custom1">
+                    <value>0</value>
+                    <value>1</value>
+                    <value>2</value>
+                    <value>3</value>
+                    <value>4</value>
+                </customDataType>
+                <customDataType name="custom2">
+                    <value>5</value>
+                    <value>6</value>
+                    <value>7</value>
+                    <value>8</value>
+                    <value>9</value>
+                </customDataType>
+            </configuration>
+            """,
+            result);
+    }
 }
--- FakeMaker.Core/ConfigurationSerializer.cs
+++ FakeMaker.Core/ConfigurationSerializer.cs
@@ -13,9 +13,17 @@ public static class ConfigurationSerializer
         var documentElement = xmlDocument.CreateElement("configuration");
         xmlDocument.AppendChild(documentElement);
 
-        if (configuration.Columns.Count > 0)
+        if (configuration.Columns.Count > 0 || configuration.CustomDataTypes.Count > 0)
             documentElement.AppendChild(xmlDocument.CreateWhitespace(newLine));
 
+        SaveColumns(configuration, newLine, indentation, xmlDocument, documentElement);
+        SaveCustomDataTypes(configuration, newLine, indentation, xmlDocument, documentElement);
+
+        xmlDocument.Save(stream);
+    }
+
+    private static void SaveColumns(Configuration configuration, string newLine, string indentation, XmlDocument xmlDocument, XmlElement documentElement)
+    {
         foreach (var column in configuration.Columns)
         {
             var columnElement = xmlDocument.CreateElement("column");
@@ -26,7 +34,31 @@ public static class ConfigurationSerializer
             documentElement.AppendChild(columnElement);
             documentElement.AppendChild(xmlDocument.CreateWhitespace(newLine));
         }
+    }
 
-        xmlDocument.Save(stream);
+    private static void SaveCustomDataTypes(Configuration configuration, string newLine, string indentation, XmlDocument xmlDocument, XmlElement documentElement)
+    {
+        foreach (var customDataType in configuration.CustomDataTypes)
+        {
+            var customDataTypeElement = xmlDocument.CreateElement("customDataType");
+            customDataTypeElement.SetAttribute("name", customDataType.Name);
+
+            documentElement.AppendChild(xmlDocument.CreateWhitespace(indentation));
+            documentElement.AppendChild(customDataTypeElement);
+            documentElement.AppendChild(xmlDocument.CreateWhitespace(newLine));
+            customDataTypeElement.AppendChild(xmlDocument.CreateWhitespace(newLine));
+            customDataTypeElement.AppendChild(xmlDocument.CreateWhitespace(indentation));
+
+            foreach (var value in customDataType.Values)
+            {
+                var valueElement = xmlDocument.CreateElement("value");
+                valueElement.InnerText = value;
+
+                customDataTypeElement.AppendChild(xmlDocument.CreateWhitespace(indentation));
+                customDataTypeElement.AppendChild(valueElement);
+                customDataTypeElement.AppendChild(xmlDocument.CreateWhitespace(newLine));
+                customDataTypeElement.AppendChild(xmlDocument.CreateWhitespace(indentation));
+            }
+        }
     }
 }
